c4dbbf7648e77cc14a38ecad5c5cdf1a07e11a26
Jared Ignacio Alonzo <86431829+AlonzoJI@users.noreply.github.com>
2023-10-29 14:28:28 -0400

    Update qrcreator.py

--- qrcreator.py
+++ qrcreator.py
@@ -1,6 +1,13 @@
 import segno
 
-qrcode = segno.make_qr("Door_1", boost_error = True)
-qrcode2 = segno.make_qr("Door_2", boost_error = True)
+# Create a QR code with the data "Door_1" and enable error correction (boost_error)
+qrcode = segno.make_qr("Door_1", boost_error=True)
+
+# Create another QR code with the data "Door_2" and enable error correction (boost_error)
+qrcode2 = segno.make_qr("Door_2", boost_error=True)
+
+# Save the first QR code as "door1.png" with a scale of 50 (higher scale results in larger QR codes)
 qrcode.save("door1.png", scale=50)
-qrcode2.save("door2.png", scale=50)
+
+# Save the second QR code as "door2.png" with a scale of 50
+qrcode2.save("door2.png", scale=50
